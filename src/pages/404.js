1dd4115d715a5387c3a7e74b21b6fcd4f5355d9a
Bryan Benson <bryanbenson83@gmail.com>
2018-07-01 17:09:26 -0400

    Fix 404 image

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-  	<img src={'https://static.cardmarket.com/img/1bedc2ed3237d4fe58ffd2ecbac91f6d/cards/Signature_Spellbook_Jace/counterspell.jpg'} alt="counterspell" />;
+  	<img src={'https://s3.amazonaws.com/a-nerds-word/counterspell.jpeg'} alt="counterspell" />;
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist...but I won't let your sadness resolve, so at least you have that.</p>
   </div>
